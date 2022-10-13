6d919162c28a1507eab23f260eafb47b33f4a3ed
Anton <Anton_netsvetaev@mail.ru>
2022-10-13 20:47:48 +0300

    first commit

---
+++ home_work.py
@@ -0,0 +1,14 @@
+a = ["adffjl","bnnj","cmn", 15, 12, 33]
+str_list = []
+int_list = []
+with open("work.txt", "w") as f:
+    for i in a:
+        if type(i) == str:
+           str_list.append(i)
+        elif type(i) == int:
+            int_list.append(i)
+
+print()
+
+
+
